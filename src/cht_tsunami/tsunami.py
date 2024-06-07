fc3b47fffb1c4721ac7ada8699f9e45000d2948a
maartenvanormondt <maarten.vanormondt@deltares.nl>
2024-06-07 10:02:17 -0400

    some updates

--- src/cht_tsunami/tsunami.py
+++ src/cht_tsunami/tsunami.py
@@ -1,27 +1,46 @@
 import xarray as xr
+import numpy as np
 
 from clawpack.geoclaw import dtopotools
 
 class Tsunami:
-    def __init__(self, file_name=None):
-        if file_name is not None:
-            # Read input file
-            self.read_input(file_name)
+    def __init__(self, source_file=None):
+        if source_file is not None:
+            # Read input file (assuming it is a SIFT fault for now)
+            self.read_fault(source_file)
             # Compute the initial water surface displacement
-            self.compute(file_name)
+            self.compute()
 
-    def read_input(self, file_name):
+    def read_fault(self, source_file):
         """Use clawpack scripts to read in the input file and store the data in the class"""
-        pass
+        self.fault = dtopotools.Fault()
+        # Assume for now that this is a CSV fault
+        column_map = {"longitude":0, "latitude":1, "depth":2, "length":3, "width":4, "strike":5,  "dip":6, "rake":7, "slip":8}
+        input_units = {'length': 'km', 'width': 'km', 'depth': 'km', 'slip': 'm', 'mu': 'Pa'}
+        self.fault.read(source_file, column_map, skiprows=1, delimiter=",", input_units=input_units)
+        self.fault.plot_subfaults()
+
+#        # Assume for now that this is a SIFT fault
+#        self.fault = dtopotools.SiftFault(source_file=source_file)
 
     def compute(self, dx=100.0, dy=100):
         """Compute tsunami with Okada (1985). Store initial water surface displacement in XArray"""
+        x, y = self.fault.create_dtopo_xy()
+        dtopo = self.fault.create_dtopography(x, y)
 
+        # Take the last displacement
+        dZ = np.squeeze(dtopo.dZ[-1,:,:])
+        x, y = np.meshgrid(dtopo.x, dtopo.y)
+
+        # Create an xarray dataset with the surface displacement dtopo.dZ. X and Y are the coordinates dtopo.x and dtopo.y         
         ds = xr.Dataset()
-        ds['z'] = xr.DataArray([0.0], dims=['x', 'y'])
+        ds['dZ'] = xr.DataArray(dZ, dims=['x', 'y'])
+        ds['x'] = xr.DataArray(x, dims=['x', 'y'])
+        ds['y'] = xr.DataArray(y, dims=['x', 'y'])
+        # plt = dtopo.plot_dZ_colors(0.0)
+        # plt.draw()
 
-        self.dataset = ds
-        pass
+        self.data = ds
 
     def interpolate(self, mesh):
         # mesh is an xugrid grid object, return xugrid data array
